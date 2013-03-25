99e4b1e6cb617561e404a1168d72b0c0e0b33bb4
Carsten Klee <carsten.klee@sbb.spk-berlin.de>
2013-03-25 12:16:16 +0100

    curl default is false

--- SRUSearch.php
+++ SRUSearch.php
@@ -18,6 +18,7 @@ class SRUSearch {
 	public $proxy_url;
 	public $proxy_port;
 	public $searchURL;
+	public $curluse = false;
 	
 	/*
 	 * Set the base url
@@ -49,10 +50,17 @@ class SRUSearch {
 	}
 	/*
 	* Set the proxy port if one
-	* @param $pp String
+	* @param $pp int
 	*/
 	public function setProxyPort($pp){
 		$this->proxy_port = $pp;
+	}	
+	/*
+	* Set the to true to use curluse
+	*  @param $bool bool
+	*/
+	public function setCurlUse($bool){
+		$this->curluse = $bool;
 	}
 	/*
 	* internal method to build the url
@@ -86,39 +94,50 @@ class SRUSearch {
 	*/	
 	public function searchRetrieve($query){
 		$this->buildSearchURL($query);
-		$this->_search();
+		return $this->_search();
 	}
 	/**
 	 * internal method that does the search
 	 * @return $output the SRU response
 	 */
 	private function _search(){
-		try{
-			// create curl resource
-			$ch = curl_init();
-			// set url
-			curl_setopt($ch, CURLOPT_URL, $this->searchURL);
-			if(isset($this->proxy_url) && isset($this->proxy_port)){
-				// set Proxy
-				curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, TRUE);
-				curl_setopt($ch, CURLOPT_PROXYPORT, $this->proxy_port);
-				curl_setopt($ch, CURLOPT_PROXY, $this->proxy_url);
+		if($this->curluse){
+			try{
+				// create curl resource
+				$ch = curl_init();
+				// set url
+				curl_setopt($ch, CURLOPT_URL, $this->searchURL);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+				curl_setopt($ch, CURLOPT_HEADER, true);
+				if(isset($this->proxy_url) && isset($this->proxy_port)){
+					// set Proxy
+					curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, TRUE);
+					curl_setopt($ch, CURLOPT_PROXYPORT, $this->proxy_port);
+					curl_setopt($ch, CURLOPT_PROXY, $this->proxy_url);
+				}
+				// $output contains the output string
+				$output = curl_exec($ch);
+				if(curl_errno($ch))
+				{
+					throw new Exception(curl_error($ch));
+				}
+				// close curl resource to free up system resources
+				curl_close($ch);
+				// return output
+				return $output;
+			} catch(Exception $e){
+				echo $e->getMessage();
+				die;
 			}
-			// $output contains the output string
-			$output = curl_exec($ch);
-			if(curl_errno($ch))
-			{
-				throw new Exception(curl_error($ch));
+		}
+		else {
+			if ($stream = fopen($this->searchURL, 'r')) {
+				return stream_get_contents($stream);
+				fclose($stream);
 			}
-			// close curl resource to free up system resources
-			curl_close($ch);
-			// return output
-			return $output;
-		} catch(Exception $e){
-			echo $e->getMessage();
-			die;
+			
 		}
 	}
-
 }
 ?>
